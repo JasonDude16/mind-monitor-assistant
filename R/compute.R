e1d42d9e9a4b7576312966780e605ebebe2e84d8
Jason Dude <jasondude16@gmail.com>
2023-11-16 17:46:28 -0600

    refactor PSG function and sub-functions

--- R/compute.R
+++ R/compute.R
@@ -1,28 +1,91 @@
-compute_psd <- function(df, srate, window, chans, method = "fft", scale = "amp") {
+# FFT normalization: 
+## 1) normalize by length of signal 
+## 2) multiply by 2 because amplitude is split between positive and negative freqs 
+# https://www.udemy.com/course/solved-challenges-ants/learn/lecture/17322760#overview
+
+fft_wrap <- function(x, srate) {
   
+  if (!is.data.frame(x) || !is.matrix(x)) {
+    x <- as.matrix(x)
+  } 
+  
+  N <- nrow(x)
+  hz <- seq(0, srate / 2, length.out = floor(N / 2) + 1)
+  
+  fc <- mvfft(as.matrix(x))
+  fc <- fc / N
+  spectrm <- apply(fc, 2, function (x) 2*abs(x[1:length(hz)]))
+  spectrm <- tibble::as_tibble(spectrm)
+  
+  return(list("spectrum" = spectrm, "hz" = hz))
+  
+}
+
+pwelch <- function(x, srate, winlen = 4*srate, overlap = .5) {
+
+  if (!is.matrix(x) || !is.data.frame(x)) {
+    x <- as.matrix(x)
+  }
+  
+  # hann taper
+  hwin <- .5 * (1-cos(2*pi*(1:winlen) / (winlen-1)))
+  
+  # get freqs and windows to loop over
+  hz <- seq(0, srate/2, length.out = floor(winlen/2) + 1)
+  windows <- seq(1, nrow(x) - winlen, winlen - winlen*overlap)
+  
+  nbins <- 1
+  welchspect <- matrix(0, ncol = ncol(x), nrow = length(hz))
+  for (wini in round(windows)) {
+    # ensure the input stays a matrix 
+    xsub <- as.matrix(x[wini:(wini + winlen - 1), ])
+    
+    # apply hanning to taper edges of window
+    xsub_hann <- apply(xsub, 2, function(x) x * hwin)
+    
+    # apply fft and add to welchspect
+    fc <- mvfft(as.matrix(xsub_hann)) / winlen
+    welchspect <- welchspect + 2*abs(fc[1:length(hz), ]) 
+    nbins <- nbins + 1
+  }
+  
+  # average welchspect across bins
+  welchspect <- welchspect / nbins
+  welchspect <- tibble::as_tibble(welchspect)
+  
+  return(list("spectrum" = welchspect, "hz" = hz))
+}
+
+compute_psd <- function(df, srate, window, chans, scale = "amp", method = "welch", ...) {
+
   if (!any(scale %in% c("amp", "power", "dB"))) {
     stop("Options for scale are: `amp`, `power`, `dB`")
   }
   
+  if (!any(method %in% c("fft", "welch"))) {
+    stop("Options for method are: `fft`, `welch`")
+  }
+  
   win <- window * srate
-  win <- (win[1] + 1):win[2]
+  win <- (win[1] + 1):win[length(win)]
   
   # subset to window and channels
   x <- tidyr::drop_na(tibble::as_tibble(df[win, chans]))
   
+  if (method == "welch") {
+    psd <- pwelch(x, srate, ...)
+    hz <- psd$hz
+    spectrm <- psd$spectrum
+  }
+
   if (method == "fft") {
-    # linearly spaced freqs to nyquist 
-    hz <- seq(0, srate / 2, length.out = floor(nrow(x) / 2) + 1)
-    fc <- Re(fft(as.matrix(x)))
-    # normalization: multiply by 2 because amplitude is split between positive and negative 
-    # frequencies for real-valued signal; 0 and nyquist should not be doubled 
-    # https://www.udemy.com/course/solved-challenges-ants/learn/lecture/17322760#overview
-    fc <- tibble::as_tibble(fc / nrow(x))
-    spectrm <- purrr::map_df(fc, ~ c(.x[1], 2*abs(.x[-c(1, length(.x))]), .x[length(.x)])) 
+    psd <- fft_wrap(x, srate)
+    hz <- psd$hz
+    spectrm <- psd$spectrum
   }
   
   if (scale == "power") {
-    spectrm <- tibble::as_tibble(spectrm^2)
+    spectrm <- tibble::as.tibble(spectrm^2)
     
   } else if (scale == "dB") {
     spectrm <- tibble::as_tibble(10*log10(spectrm^2))
